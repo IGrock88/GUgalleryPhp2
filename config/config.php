b1060a770148203753487b60ff5a3ca67c3ecc49
IGrock88 <igrock88@gmail.com>
2018-02-24 12:54:09 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//db
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'geekbrains');
+
+//image dir
+define('BIG_IMG_DIR', 'img/big/');
+define('SMALL_IMG_DIR', 'img/small/');
+define('GALLERY_DB_TABLE', 'img');
